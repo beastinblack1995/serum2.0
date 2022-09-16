a78c25775441e270aeb334ca0f2561197d6106b2
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-09-16 10:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,11 +266,9 @@ def givlis_df(dictionin):
         df['Australoid'] = [1]  
       
         
-    if   (dictionin['Race'] == 'Latino') :
+    if   (dictionin['Race'] == 'Latino Hispanic') or (dictionin['Race'] == 'Middle Eastern'):
         df['Caucasoid'] = [1]
-        
-    if  (dictionin['Race'] == 'Middle Eastern') :
-        df['Hispanic'] = [1]        
+         
         
     if  (dictionin['Race'] == 'Black Skin') :
         df['Negroid'] = [1] 
